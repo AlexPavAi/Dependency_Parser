79206d61b387d302bdcab6642a6769b69314260f
noamwolf <noamvolf@gmail.com>
2020-06-05 17:21:53 +0300

    network prototype update

--- net_prototype.py
+++ net_prototype.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torch.functional import F
 import torch
 
 class net_prototype(nn.Module):
@@ -9,14 +10,8 @@ class net_prototype(nn.Module):
         self.tag_embedding = nn.Embedding(tag_vocab_size, tag_emb_dim)    # (B, len(sentence))
         self.lstm = nn.LSTM(input_size=word_emb_dim + tag_emb_dim, hidden_size=lstm_hidden_dim,
                             batch_first=True, bidirectional=True)  # (B, len(sentence), 2 * hidden)
-        self.layer1_head = nn.Sequential(
-            nn.Linear(2 * lstm_hidden_dim, mlp_hidden_dim),
-            nn.Tanh()
-        )  # (B, mlp_hidden_dim ,len(sentence))
-        self.layer1_modifier = nn.Sequential(
-            nn.Linear(2 * lstm_hidden_dim, mlp_hidden_dim),
-            nn.Tanh()
-        )  # (B, mlp_hidden_dim ,len(sentence))
+        self.layer1_head = nn.Linear(2 * lstm_hidden_dim, mlp_hidden_dim)  # (B, mlp_hidden_dim ,len(sentence))
+        self.layer1_modifier = nn.Linear(2 * lstm_hidden_dim, mlp_hidden_dim)  # (B, mlp_hidden_dim ,len(sentence))
         self.out_layer = nn.Linear(mlp_hidden_dim, 1)
 
     def forward(self, word_idx, tag_idx):
@@ -26,12 +21,11 @@ class net_prototype(nn.Module):
         lstm_out, _ = self.lstm(x)
         vh = self.layer1_head(lstm_out)
         vm = self.layer1_modifier(lstm_out)
-        vh = self.out_layer(vh)
-        vm = self.out_layer(vm)
-        vh = vh.repeat(1, vh.shape[1], 1)
-        vm = vm.repeat(1, vm.shape[1], 1)
+        vh = vh.repeat(1, vh.shape[1], 1).view(1, vh.shape[1], vh.shape[1], -1)
+        vm = vm.repeat(1, vm.shape[1], 1).view(1, vm.shape[1], vm.shape[1], -1)
         vm = vm.transpose(1, 2)
         out = vh + vm
+        out = self.out_layer(out)
         out = out[:, :, 1:]
         return out
 
