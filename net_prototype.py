b28966acce360546fd77fe72f7108ae8a5e8ce7b
noamwolf <noamvolf@gmail.com>
2020-06-07 12:46:32 +0300

    network prototype update

--- net_prototype.py
+++ net_prototype.py
@@ -10,8 +10,8 @@ class net_prototype(nn.Module):
         self.tag_embedding = nn.Embedding(tag_vocab_size, tag_emb_dim)    # (B, len(sentence))
         self.lstm = nn.LSTM(input_size=word_emb_dim + tag_emb_dim, hidden_size=lstm_hidden_dim,
                             batch_first=True, bidirectional=True)  # (B, len(sentence), 2 * hidden)
-        self.layer1_head = nn.Linear(2 * lstm_hidden_dim, mlp_hidden_dim)  # (B, mlp_hidden_dim ,len(sentence))
-        self.layer1_modifier = nn.Linear(2 * lstm_hidden_dim, mlp_hidden_dim)  # (B, mlp_hidden_dim ,len(sentence))
+        self.layer1_head = nn.Linear(2 * lstm_hidden_dim, mlp_hidden_dim)  # (B, len(sentence), mlp_hidden_dim)
+        self.layer1_modifier = nn.Linear(2 * lstm_hidden_dim, mlp_hidden_dim)  # (B, len(sentence), mlp_hidden_dim)
         self.out_layer = nn.Linear(mlp_hidden_dim, 1)
 
     def forward(self, word_idx, tag_idx):
@@ -21,12 +21,12 @@ class net_prototype(nn.Module):
         lstm_out, _ = self.lstm(x)
         vh = self.layer1_head(lstm_out)
         vm = self.layer1_modifier(lstm_out)
-        vh = vh.repeat(1, vh.shape[1], 1).view(1, vh.shape[1], vh.shape[1], -1)
-        vm = vm.repeat(1, vm.shape[1], 1).view(1, vm.shape[1], vm.shape[1], -1)
-        vm = vm.transpose(1, 2)
+        vh = vh.repeat(1, vh.shape[1], 1).view(vh.shape[0], vh.shape[1], vh.shape[1], -1)
+        vh = vh.transpose(1, 2)
+        vm = vm.repeat(1, vm.shape[1], 1).view(vm.shape[0], vm.shape[1], vm.shape[1], -1)
         out = vh + vm
         out = F.tanh(out)
-        out = self.out_layer(out)
+        out = self.out_layer(out).squeeze(3)
         out = out[:, :, 1:]
         return out
 
