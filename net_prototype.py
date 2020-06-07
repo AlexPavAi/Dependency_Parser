a182ac205edde999574e3d506ff74efd8f7b52da
noamwolf <noamvolf@gmail.com>
2020-06-07 12:47:14 +0300

    network prototype update

--- net_prototype.py
+++ net_prototype.py
@@ -8,7 +8,7 @@ class net_prototype(nn.Module):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.word_embedding = nn.Embedding(word_vocab_size, word_emb_dim)  # (B, len(sentence))
         self.tag_embedding = nn.Embedding(tag_vocab_size, tag_emb_dim)    # (B, len(sentence))
-        self.lstm = nn.LSTM(input_size=word_emb_dim + tag_emb_dim, hidden_size=lstm_hidden_dim,
+        self.lstm = nn.LSTM(input_size=word_emb_dim + tag_emb_dim, hidden_size=lstm_hidden_dim, num_layers=2,
                             batch_first=True, bidirectional=True)  # (B, len(sentence), 2 * hidden)
         self.layer1_head = nn.Linear(2 * lstm_hidden_dim, mlp_hidden_dim)  # (B, len(sentence), mlp_hidden_dim)
         self.layer1_modifier = nn.Linear(2 * lstm_hidden_dim, mlp_hidden_dim)  # (B, len(sentence), mlp_hidden_dim)
