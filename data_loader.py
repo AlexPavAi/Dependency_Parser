6fee2f496c3c82d5ed669c6b60a1589bcc229805
noamwolf <noamvolf@gmail.com>
2020-06-07 21:10:36 +0300

    added root to data loader

--- data_loader.py
+++ data_loader.py
@@ -1,27 +1,19 @@
-import pandas as pd
-import numpy as np
 import os
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import random
-from torch.utils.data.dataloader import DataLoader
-import collections
 from torchtext.vocab import Vocab
-from torch.utils.data.dataset import Dataset, TensorDataset
-from pathlib import Path
+from torch.utils.data.dataset import Dataset
 from collections import Counter
 from collections import defaultdict
 
 UNKNOWN_TOKEN = "<unk>"
-PAD_TOKEN = "<pad>" # Optional: this is used to pad a batch of sentences in different lengths.
-SPECIAL_TOKENS = [PAD_TOKEN, UNKNOWN_TOKEN]
+ROOT_TOKEN = "<ROOT>"  # Optional: this is used to pad a batch of sentences in different lengths.
+SPECIAL_TOKENS = [UNKNOWN_TOKEN, ROOT_TOKEN]
+
 
 def get_vocabs(file_path):
     """
         Extract vocabs from given datasets. Return a word2ids and tag2idx.
-        :param file_paths: a list with a full path for all corpuses
+        :param file_path: full path of the corpuses
             Return:
               - word2idx
               - tag2idx
@@ -39,7 +31,7 @@ def get_vocabs(file_path):
                 pos_dict[pos_tag] += 1
 
     index_dict_word = Vocab(Counter(word_dict), specials=SPECIAL_TOKENS)
-    index_dict_pos = Vocab(Counter(pos_dict), specials=SPECIAL_TOKENS)
+    index_dict_pos = Vocab(Counter(pos_dict), specials=[ROOT_TOKEN])
     return word_dict, pos_dict, index_dict_word.stoi, index_dict_pos.stoi
 
 
@@ -62,7 +54,7 @@ class PosDataReader:
                     word = splited_words[1]
                     pos_tag = splited_words[3]
                     head_index = splited_words[6]
-                    cur_sentence.append((word,pos_tag,head_index))
+                    cur_sentence.append((word, pos_tag, head_index))
                 else:
                     self.sentences.append(cur_sentence)
                     cur_sentence = []
@@ -78,11 +70,11 @@ class PosDataset(Dataset):
         super().__init__()
         self.subset = subset  # One of the following: [train, test]
         # self.file = dir_path + subset + ".labeled"
-        self.file = os.path.join(dir_path,subset)+".labeled"
+        self.file = os.path.join(dir_path, subset) + ".labeled"
         self.datareader = PosDataReader(self.file)
         # self.vocab_size = len(self.datareader.word_dict)
         _, _, self.word_idx_mappings, self.pos_idx_mappings = get_vocabs(self.file)
-        self.sentences_dataset = self.convert_sentences_to_dataset(padding)
+        self.sentences_dataset = self.convert_sentences_to_dataset()
 
     def __len__(self):
         return len(self.sentences_dataset)
@@ -91,17 +83,16 @@ class PosDataset(Dataset):
         word_embed_idx, pos_embed_idx, head, sentence_len = self.sentences_dataset[index]
         return word_embed_idx, pos_embed_idx, head, sentence_len
 
-
-    def convert_sentences_to_dataset(self, padding):
+    def convert_sentences_to_dataset(self):
         sentence_word_idx_list = list()
         sentence_pos_idx_list = list()
         sentence_head_list = list()
         sentence_len_list = list()
         for sentence_idx, sentence in enumerate(self.datareader.sentences):
-            words_idx_list = []
-            pos_idx_list = []
+            words_idx_list = [self.word_idx_mappings[ROOT_TOKEN]]
+            pos_idx_list = [self.pos_idx_mappings[ROOT_TOKEN]]
             head_idx_list = []
-            for word, pos, head in sentence: #TODO: pay attention, root is not in the sentence, maybe the indexing of the heads has to be changed
+            for word, pos, head in sentence:
                 words_idx_list.append(self.word_idx_mappings.get(word))
                 pos_idx_list.append(self.pos_idx_mappings.get(pos))
                 head_idx_list.append(head)
@@ -116,13 +107,15 @@ class PosDataset(Dataset):
                                                                      sentence_head_list,
                                                                      sentence_len_list))}
 
+
 def main():
     data_dir = "data"
     # get_vocabs(list_of_pathes)
-    dir_path ="data"
-    str ="train"
-    train = PosDataset(data_dir,str, padding=False)
+    dir_path = "data"
+    str = "train"
+    train = PosDataset(data_dir, str)
     print("")
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     main()
